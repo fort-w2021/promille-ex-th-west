e79725d15d7f6b397e5f0f03a3e89757ca73fd9f
th-west <twestermeier@live.de>
2020-11-08 11:51:30 +0100

    in progress

---
+++ topdown-promille-ex-sol.R
@@ -0,0 +1,133 @@
+#' Function to check age and drinks
+#' @param age age of that person
+#' @param drinks list or named vector of drinks
+
+#' @return possible warning message due to illegal activity
+
+checking_age <- function(age, drinks) {
+  drinks <- unlist(drinks)
+  age_class <- ifelse("schnaps" %in% names(drinks), "adult", "minor")
+  if (age_class == "adult" & age < 18) {
+    warning(" Consumption of hard liquor for minors is illegal")
+  }
+
+  if (age_class == "minor" & age < 16) {
+    warning("Consumption of alcohol for minors younger than 16 is illegal")
+  }
+  # drinks <- match.arg(names(drinks),
+  #   choices = c("massn", "hoibe", "wein", "schnaps"), several.ok = TRUE
+  # )
+  # switch(drinks,
+  #   hoibe = "minor",
+  #   massn = "minor",
+  #   wein = "minor",
+  #   schnaps = "adult",
+  # )
+}
+
+#' Function that computes the mass of alcohol in grams
+#' @param drinks a list or named vector containing  alcoholic beverages
+#' and their count
+#'
+#' @return the total amount of alcohol in grams
+calculate_mass <- function(drinks) {
+  mass_sum <- 0
+  drinks <- unlist(drinks)
+  alcohol_mass_list <- list(
+    "hoibe" = 500 * 0.06 * 0.8,
+    "massn" = 1000 * 0.06 * 0.8,
+    "wein" = 200 * 0.11 * 0.8,
+    "schnaps" = 40 * 0.4 * 0.8
+  )
+
+  for (i in names(drinks)) {
+    mass_sum <- mass_sum + alcohol_mass_list[[i]] * as.vector(drinks[i])
+  }
+
+  mass_sum
+}
+
+#' Function to transform different inputs of variable sex
+#' @param sex a character declaring sex of a person
+
+#' @return value female or male for sex
+getting_gender_right <- function(sex = c("male", "m", "M", "female", "f", "F")) {
+  sex <- tolower(sex)
+  sex <- match.arg(sex)
+  switch(sex,
+    male = "male",
+    m = "male",
+    female = "female",
+    f = "female"
+  )
+}
+
+#' Function that calculates total body water
+#' @param age the age of a specific person
+#' @param sex the gender of that person
+#' @param height height in cm
+#' @param weight weight in kg
+
+#' @returntotal body water numeric
+calculate_gkw <- function(age, sex = c("male", "female"), height, weight) {
+  sex <- getting_gender_right(sex = sex)
+  sex <- match.arg(sex)
+  switch(sex,
+    female = 0.203 - 0.07 * age + 0.1069 * height + 0.2466 * weight,
+    male = 2.447 - 0.09516 * age + 0.1074 * height + 0.3362 * weight
+  )
+}
+
+
+#' Function that calculates blood alcohol level
+#' @param  gkw total body water
+#' @param  alc_mass mass of alcohol in grams
+
+#' @return  blood alcohol level per thousand
+calculate_blood_alcohol <- function(gkw, alc_mass) {
+  blood_level <- (0.8 * alc_mass) / (1.055 * gkw)
+  blood_level
+}
+
+#' Function that calculates blood alcohol level decline during drinking
+#' starting from second hour after start
+#' @param  blood_level blood level of alcohol per thousand
+#' @param  drinking_time vector of POSIXct Values.
+
+#' @return  final level of blood alcohol at the end of drinking time
+calculate_blood_alcohol_final <- function(blood_level, drinking_time) {
+  time <- (as.numeric(drinking_time[2]) - as.numeric(drinking_time[1])) / 3600
+  if (time >= 1) {
+    blood_level <- blood_level - ((time - 1) * 0.15)
+  }
+  max(0, blood_level)
+}
+
+
+#' Function that calculates the blood level of alcohol after some time of drinking
+#' @param age the age of the person
+#' @param sex the gender of the person
+#' @param height height in cm
+#' @param weight weight in kg
+#' @param drinking_time a vector of class POSIXct and length 2 with start and
+#'                      end time of drinking
+#' @param drinks a list or named vector with different drinks and their count
+
+#' @return the final blood level of alcohol per thousand
+tell_me_how_drunk <- function(age, sex = c("male", "female"), height, weight,
+                              drinking_time, drinks) {
+  
+  checking_age(age = age, drinks = drinks)
+  mass <- calculate_mass(drinks = drinks)
+  body_water <- calculate_gkw(
+    age = age, sex = sex, height = height,
+    weight = weight
+  )
+  blood_level <- calculate_blood_alcohol(gkw = body_water, alc_mass = mass)
+  blood_level_final <- calculate_blood_alcohol_final(
+    blood_level = blood_level,
+    drinking_time = drinking_time
+  )
+
+  blood_level_final
+}
